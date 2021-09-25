dee6e451a1f65987822599574279ff4f7eba956f
Karim Sonbol <karim.sonbol@adevinta.com>
2021-09-25 21:24:07 +0200

    add practitioner app

--- digitaltwin/digitaltwin/settings.py
+++ digitaltwin/digitaltwin/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'twin.apps.TwinConfig', 
+    'practitioner.apps.PractitionerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
