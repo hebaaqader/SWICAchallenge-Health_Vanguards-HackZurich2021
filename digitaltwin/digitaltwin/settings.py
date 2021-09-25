b4326c063eed58889f279c86c7a1bd2837f18a18
Karim Sonbol <karim.sonbol@adevinta.com>
2021-09-25 14:44:02 +0200

    add postgres setup

--- digitaltwin/digitaltwin/settings.py
+++ digitaltwin/digitaltwin/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'digitaltwin.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hackathon',
+        'USER': 'hackathon',
+        'PASSWORD': 'h5SyzAhH',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
